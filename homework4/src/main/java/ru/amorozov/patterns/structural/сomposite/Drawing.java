943ca7bafa91ae0f19b9c40387f5ffb7cd9acea0
MorozovGeeK <morozov4811@gmail.com>
2021-07-06 00:08:20 +0400

    hw-4

---
+++ homework4/src/main/java/ru/amorozov/patterns/structural/сomposite/Drawing.java
@@ -0,0 +1,24 @@
+package ru.amorozov.patterns.structural.сomposite;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Drawing implements Car {
+    private List<Car> cars = new ArrayList<Car>();
+
+    public void draw(String color) {
+        for (Car car : cars) {
+            car.draw(color);
+        }
+    }
+
+    public void add(Car s) {
+        this.cars.add(s);
+    }
+
+    public void clear() {
+        System.out.println();
+        this.cars.clear();
+    }
+}
+
---
+++ homework4/src/main/java/ru/amorozov/patterns/structural/сomposite/SportCar.java
@@ -0,0 +1,7 @@
+package ru.amorozov.patterns.structural.сomposite;
+
+public class SportCar implements Car{
+    public void draw(String color) {
+        System.out.println("SportCar color: " + color);
+    }
+}
